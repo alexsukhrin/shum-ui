c9732ff2d18207a2280403612094798de486a475
Олександр Сухрин <o.sukhryn@temabit.com>
2025-07-26 20:00:12 +0300

    Add internationalization support for English and Ukrainian languages

---
+++ lib/l10n/app_localizations_uk.dart
@@ -0,0 +1,31 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Ukrainian (`uk`).
+class AppLocalizationsUk extends AppLocalizations {
+  AppLocalizationsUk([String locale = 'uk']) : super(locale);
+
+  @override
+  String get appTitle => 'Shum UI';
+
+  @override
+  String get counterTitle => 'Демо Flutter Головна Сторінка';
+
+  @override
+  String get counterText => 'Ви натиснули кнопку стільки разів:';
+
+  @override
+  String get incrementButton => 'Збільшити';
+
+  @override
+  String get languageSelector => 'Мова';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get ukrainian => 'Українська';
+}
